60f913d469407a66aaec76cb407d7841ff56fcc1
Bikash Raja <vikashraja2059@gmail.com>
2026-08-19 17:28:41 +0200

    chore: update sample customer data

--- legacy-service/src/main/resources/data.sql
+++ legacy-service/src/main/resources/data.sql
@@ -1,1 +1,5 @@
-d
+INSERT INTO customers (id, name, status, balance)
+VALUES
+    (10001, 'MonkeyD Luffy', 'ACTIVE', 1000.00),
+    (10002, 'Idont Know', 'ACTIVE', 2500.50),
+    (10003, 'Heis Him', 'INACTIVE', 500.00);
